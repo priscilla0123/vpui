800fdddf85eed5257b406287ebc26386af4517f8
sunjia032 <sunjia032@pingan.com.cn>
2017-09-27 10:06:23 +0800

    merge

--- examples/src/index.js
+++ examples/src/index.js
@@ -3,107 +3,73 @@ import Vue from 'vue';
 
 Vue.use(VueRouter);
 
-import Pager from './components/pager.vue';
-import Datagrid from './components/datagrid.vue';
-import Checkbox from './components/checkbox.vue';
+import Button from './components/button.vue';
 import Radio from './components/radio.vue';
-import OverlayView from './views/overlay.vue'; 
-import Tab from './components/tab.vue';
+import Checkbox from './components/checkbox.vue';
 import Select from './components/select.vue';
-import CityPicker from './views/citypicker.vue';
+
+import Tab from './components/tab.vue';
+import Pager from './components/pager.vue';
+
+import Datagrid from './components/datagrid.vue';
+
+import OverlayView from './components/overlay.vue';
+
+import CityPicker from './components/citypicker.vue';
+import DatePicker from './components/datepicker.vue';
 
 import Tablepager from './modules/tablepager.vue';
 
 import Valid from './directives/valid.vue';
 import Clickoutside from './directives/clickoutside.vue';
 
+// COMPONENTS
+const com = [
+    // elements
+    { path: '/components/button', component: Button, text: 'Button', id: 'com_1' },
+    { path: '/components/radio', component: Radio, text: 'Radio' },
+    { path: '/components/checkbox', component: Checkbox, text: 'Checkbox' },
+    { path: '/components/select', component: Select, text: 'Select'},
+
+    // groups
+    { path: '/components/pager', component: Pager, text: 'Pager' },
+    { path: '/components/tab', component: Tab, text: 'Tab' },
+
+    // data
+    { path: '/components/datagrid', component: Datagrid, text: 'Datagrid' },
+
+    // overlays
+    { path: '/components/overlay', component: OverlayView, text: 'OverlayView' },
+
+    // pickers
+    { path: '/components/citypicker', component: CityPicker, text: 'CityPicker' },
+    { path: '/components/datepicker', component: DatePicker, text: 'DatePicker' },
+]
+
+// MODULES
+const mod = [
+    { path: '/modules/tablepager', component: Tablepager, text: 'Tablepager', id: 'mod_1' }
+]
+
+// DIRECTIVES
+const dir = [
+    { path: '/directives/valid', component: Valid, text: 'Valid', id: 'dir_1' },
+    { path:'/directives/clickoutside', component: Clickoutside, text: 'Clickoutside', id: 'dir_2' }
+]
+
 const router = new VueRouter({
-    //COMPONENTS
-    routes: [{
-        path: '/components/pager',
-        component: Pager
-    }, {
-        path: '/components/datagrid',
-        component: Datagrid
-    }, {
-        path: '/components/overlay',
-        component: OverlayView
-    }, {
-        path: '/components/citypicker',
-        component: CityPicker
-    }, {
-        path: '/components/checkbox',
-        component: Checkbox
-    }, {
-        path: '/components/radio',
-        component: Radio
-    }, {
-        path: '/components/tab',
-        component: Tab
-    },{
-        path: '/components/select',
-        component: Select
-    },{
-        //MODULES
-        path: '/modules/tablepager',
-        component: Tablepager
-    }, {
-        //DIRECTIVES
-        path: '/directives/valid',
-        component: Valid
-    },{
-        //DIRECTIVES
-        path:'/directives/clickoutside',
-        component: Clickoutside
-    }]
-});
+    routes: [...com, ...mod, ...dir].map(({ path, component }) => ({ path, component }))
+})
 
 new Vue({
     el: '#app',
     router: router,
     data() {
         return {
-            com: [{
-                text: 'Radio',
-                url: '#/components/radio'
-            }, {
-                text: 'Checkbox',
-                url: '#/components/checkbox'
-            }, {
-                text: 'Pager',
-                url: '#/components/pager',
-                id: 'com_1'
-            }, {
-                text: 'Datagrid',
-                url: '#/components/datagrid',
-                id: 'com_2'
-            }, {
-                text: 'Overlay',
-                url: '#/components/overlay'
-            }, {
-                text: 'Tab',
-                url: '#/components/tab'
-            }, {
-                text: 'Select',
-                url: '#/components/select'
-            }, {
-                text: 'CityPicker',
-                url: '#/components/citypicker'
-            }],
-            mod: [{
-                text: 'Tablepager',
-                url: '#/modules/tablepager',
-                id: 'mod_1'
-            }],
-            dir: [{
-                text: 'Valid',
-                url: '#/directives/valid',
-                id: 'dir_1'
-            }, {
-                text: 'Clickoutside',
-                url: '#/directives/clickoutside'
-            }],
-            active: 'com_1'
+            active: 'com_1',
+            com: com.map(({ text, path }) => ({ url: '#' + path, text })),
+            mod: mod.map(({ text, path }) => ({ url: '#' + path, text })),
+            dir: dir.map(({ text, path }) => ({ url: '#' + path, text }))
         }
     },
     method: {
@@ -111,4 +77,4 @@ new Vue({
             this.active = id;
         }
     }
-});
+})
---
+++ src/components/timeline/timeline.vue
@@ -0,0 +1,286 @@
+<template>
+    <div class="lg-pager" :class="klass" v-if="showPager">        
+        <ul>                    
+            <li class="lg-pager-item lg-pager-previous" :class="{'disable': isHead}">
+                <a href="javascript:" @click="to(pager.current-1)"></a>
+            </li>
+            <li class="lg-pager-item" :class="{'lg-pager-current': isHead}">
+                <a href="javascript:" @click="to(1)">1</a>
+            </li>
+            <li class="lg-pager-item lg-pager-dot" v-if="pager.start != 2">
+                ...
+            </li>
+            <li class="lg-pager-item" v-for="n in (pager.end-pager.start + 1)" :class="{'lg-pager-current': pager.current == (pager.start + n - 1)}">
+                <a href="javascript:" @click="to(pager.start + n - 1)">{{pager.start + n - 1}}</a>
+            </li>
+            <li class="lg-pager-item lg-pager-dot" v-if="pager.end < pager.total - 1">
+                ...
+            </li>
+            <li class="lg-pager-item" :class="{'lg-pager-current': isTail}" v-if="pager.total > 1">
+                <a href="javascript:" @click="to(pager.total)">{{pager.total}}</a>
+            </li>
+            <li class="lg-pager-item lg-pager-next" :class="{'disable': isTail}">
+                <a href="javascript:" @click="to(pager.current + 1)"></a>
+            </li>
+            <li class="lg-pager-shortcut">
+                去第<input type="text" v-model="shortcut">页<a href="javascript:" class="lg-pager-shortcut-confirm" @click="to(shortcut)">确定</a>
+            </li>
+            <li slot="before" v-if="$slots.before">
+                <slot name="before"></slot> 
+            </li>
+            <li class="lg-pager-total">共{{pager.total}}页</li>
+            <li slot="after" v-if="$slots.after">
+                <slot name="after"></slot> 
+            </li>             
+        </ul>         
+    </div>
+</template>
+<style>
+.lg-pager {
+    margin: 5px auto;
+    font: 12px Tahoma, Helvetica Neue, Hiragino Sans GB, Microsoft Yahei, sans-serif;
+    overflow: hidden;
+    height: 50px;
+    text-align: center;
+}
+
+.lg-pager-left {
+    text-align: left;
+}
+
+.lg-pager-right {
+    text-align: right;
+}
+
+.lg-pager ul {
+    display: inline-block;
+}
+
+.lg-pager li {
+    float: left;
+    list-style: none;
+    margin: 2px 3px;
+    line-height: 28px;
+    font-size: 12px;
+    height: 28px;
+}
+
+.lg-pager-item {
+    border: 1px solid #dfdfdf;
+    border-radius: 2px;
+    background-color: #fff;
+}
+
+.lg-pager-item:hover {
+    border-color: #5986E1;
+}
+
+.lg-pager-item.disable a {
+    color: #a3a3a3;
+}
+
+.lg-pager-item.disable a:after,
+.lg-pager-item.disable a:before {
+    color: #a3a3a3;
+    border-left-color: #a3a3a3;
+}
+
+.lg-pager-item.disable:hover {
+    border-color: #dfdfdf;
+}
+
+.lg-pager-item a {
+    display: inline-block;
+    text-align: center;
+    color: #666;
+    height: 28px;
+    min-width: 28px;
+    line-height: 28px;
+    padding: 0 5px;
+    text-decoration: none;
+}
+
+.lg-pager-total {
+    border: 1px solid transparent;
+}
+
+.lg-pager-current {
+    background: #5986E1;
+    border-color: #5986E1;
+}
+
+.lg-pager-current a {
+    color: #fff;
+}
+
+.lg-pager-item-large a {
+    padding: 0px 4px;
+}
+
+.lg-pager-previous a:before {
+    content: '';
+    display: inline-block;
+    border-top: 5px solid transparent;
+    border-bottom: 5px solid transparent;
+    border-left: 7px solid #666;
+    border-right: none;
+    -webkit-transform: rotateZ(180deg);
+    -moz-transform: rotateZ(180deg);
+    -o-transform: rotateZ(180deg);
+    -ms-transform: rotateZ(180deg);
+    transform: rotateZ(180deg);
+}
+
+.lg-pager-next a:after {
+    content: '';
+    display: inline-block;
+    border-top: 5px solid transparent;
+    border-bottom: 5px solid transparent;
+    border-left: 7px solid #666;
+    border-right: none;
+}
+
+.lg-pager-current a,
+.lg-pager-point a {
+    currentsor: default;
+}
+
+.lg-pager-shortcut {
+    height: 24px;
+    color: #a3a3a3;
+    border: 1px solid transparent;
+}
+
+.lg-pager-shortcut input {
+    height: 24px;
+    width: 38px;
+    padding: 0px;
+    outline: none;
+    text-align: center;
+    margin: 0 6px;
+    border-radius: 3px;
+    border: 1px solid #a3a3a3;
+    box-size: border-box;
+}
+
+.lg-pager-shortcut-confirm {
+    border-radius: 3px;
+    background: #5986E1;
+    text-decoration: none;
+    text-align: center;
+    display: inline-block;
+    color: #fff;
+    width: 50px;
+    height: 24px;
+    line-height: 24px;
+    margin: 0 5px;
+}
+
+.lg-pager-dot {
+    border: none;
+}
+</style>
+<script>
+var Pager = {
+    name: 'pager',
+    props: {
+        'total': {
+            type: Number,
+            require: true
+        },
+        'current': {
+            type: Number,
+            require: true,
+            default: 1
+        },
+        'position': {
+            type: String
+        },
+        'volumn': {
+            type: Number,
+            default: 10,
+            validator(value) {
+                return value > 5;
+            }
+        }
+    },
+    methods: {
+        to(current) {
+            var cur = Math.floor(Number(current));
+            if (isNaN(cur)) {
+                alert('别任性~');
+                return;
+            }
+            if (cur <= this.pager.total && cur >= 1 && cur != this.pager.current) {
+                this.calculate(cur);
+                this.$emit('to', cur);
+            }
+        },
+        calculate(current) {
+            var current = Math.floor(current / 1);
+            var start = 2,
+                end = this.pager.total - 1;
+            if (this.pager.total > this.vol) {
+                if (current - this.pre > 1) {
+                    start = current - this.pre;
+                    if (current + this.next - this.pager.total < 0) {
+                        end = current + this.next
+                    } else {
+                        start = end - (this.vol - 3);
+                    }
+                } else {
+                    end = start + this.vol - 3;
+                }
+            } else if (start > end) {
+                end = 1;
+            }
+            this.pager.start = start;
+            this.pager.end = end;
+            this.pager.current = current;
+        },
+        update() {
+            this.vol = this.volumn;
+            this.pre = Math.floor((this.vol - 3) / 2);
+            this.next = Math.ceil((this.vol - 3) / 2);
+            this.pager.total = this.total; 
+            this.calculate(this.current);
+        }
+    },
+    data() {
+        return {
+            pager: {
+                total: this.total,
+                current: this.current
+            },
+            klass: {
+                'lg-pager-left': this.position == 'left',
+                'lg-pager-right': this.position == 'right'
+            },
+            shortcut:''
+        }
+    },
+    created() {
+        this.update();
+    },
+    computed: {
+        isHead() {
+            return this.pager.current == 1;
+        },
+        isTail() {
+            return this.pager.current == this.pager.total;
+        },
+        showPager() {
+            return !!this.pager.total;
+        },
+        propsUpdate() {
+            return this.total + '&' + this.current + '&' + this.volumn;
+        }
+    },
+    watch: {
+        'propsUpdate': function() {
+            this.update();
+        }
+    }
+}
+export default Pager;
+</script>
